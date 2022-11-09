b647bb1628808d45de52b262f92ab931a2c32a30
Jordan Deuley <jordan.deuley@lyon.edu>
2022-11-09 13:52:18 -0600

    Found maybe a better way to get all the articles. Just trying to figure out how to use XPath better. XPath is the root of all my problems right now.

--- pycode/foxnews.py
+++ pycode/foxnews.py
@@ -8,25 +8,39 @@ options.add_argument('--disable-gpu')
 driver = webdriver.Chrome(options=options)
 driver.get("https://www.foxnews.com/")
 
-class FoxNews:
+
+
+class FoxNewsArticle:
     topic = ""
     title = ""
     link = ""
 
-ExclusiveClipsSection = driver.find_elements(By.XPATH, "//*[@id='wrapper']/div/div[2]/div[1]/aside[1]/div/div/div[3]/section/div/article")
-ExclusiveClips = ["" for i in range(len(ExclusiveClipsSection))]
+def FoxNewsScrape():
+
+    Articles = []
+    Entry = FoxNewsArticle
+    ExclusiveClipsSection = driver.find_elements(By.XPATH, "//*[@id='wrapper']/div/div[2]/div[1]/aside[1]/div/div/div[3]/section/div/article")
+
+    for i in range(1, len(ExclusiveClipsSection)):
+
+        Entry.topic = driver.find_element(By.XPATH, "//*[@id='wrapper']/div/div[2]/div[1]/aside[1]/div/div/div[3]/section/div/article[{}]/div[2]/header/div/span/a".format(i)).get_attribute("textContent")
+        Entry.title = driver.find_element(By.XPATH, "//*[@id='wrapper']/div/div[2]/div[1]/aside[1]/div/div/div[3]/section/div/article[{}]/div[2]/header/h2/a".format(i)).get_attribute("textContent")
+        Entry.link = driver.find_element(By.XPATH, "//*[@id='wrapper']/div/div[2]/div[1]/aside[1]/div/div/div[3]/section/div/article[{}]/div[2]/header/h2/a".format(i)).get_attribute("href")
+
+        Articles.append(Entry)
 
 
-for i in range(1, len(ExclusiveClipsSection)):
-    Entry = FoxNews
-    topic = driver.find_element(By.XPATH, "//*[@id='wrapper']/div/div[2]/div[1]/aside[1]/div/div/div[3]/section/div/article[{}]/div[2]/header/div/span/a".format(i)).get_attribute("textContent")
-    title = driver.find_element(By.XPATH, "//*[@id='wrapper']/div/div[2]/div[1]/aside[1]/div/div/div[3]/section/div/article[{}]/div[2]/header/h2/a".format(i)).get_attribute("textContent")
-    link = driver.find_element(By.XPATH, "//*[@id='wrapper']/div/div[2]/div[1]/aside[1]/div/div/div[3]/section/div/article[{}]/div[2]/header/h2/a".format(i)).get_attribute("href")
 
-    Entry.topic = topic
-    Entry.title = title
-    Entry.link = link
-    print(f"Topic: {Entry.topic}\nTitle: {Entry.title}\nLink: {Entry.link}\n")
+        return Articles
+
+
+articlesList = driver.find_elements(By.TAG_NAME, "article")
+
+
+for i in range(len(articlesList)):
+    print(articlesList[i].find_element(By.XPATH, "/div[2]/header/div/span/a".format(i)).get_attribute("textContent"))
+
+
 
 driver.close()
 
