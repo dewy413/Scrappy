5302a26ced5b51e397be74fd4ce2a859337ce296
Dewy413 <114099341+dewy413@users.noreply.github.com>
2022-11-09 09:51:32 -0600

    Messed around with XPaths. Trying to figure out a nice way to gather information. Once I get this done I should be able to get through everything else a lot quicker.

--- pycode/main.py
+++ pycode/main.py
@@ -1,18 +1,27 @@
-import selenium.common.exceptions
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
+
+
+
 options = Options()
-options.add_argument("headless") #Comment this out if you want to see it open the browser.
+#options.add_argument("headless") #Comment this out if you want to see it open the browser.
 options.add_argument('--disable-gpu')
 driver = webdriver.Chrome(options=options)
-driver.get("https://www.foxnews.com/politics")
-
-clss = driver.find_element(By.XPATH,
-                               "/html/body/div[2]/div/div/div/div[2]/div/main/section[1]/div/article[1]").text
-
-print(clss)
+driver.get("https://www.foxnews.com/")
 
 
+
+class article:
+    topic = ""
+    title = ""
+
+ExclusiveClipsSection = driver.find_elements(By.XPATH, "//*[@id='wrapper']/div/div[2]/div[1]/aside[1]/div/div/div[3]/section/div/article")
+ExclusiveClips = ['' for i in range(len(ExclusiveClipsSection))]
+
+for i in range(0, len(ExclusiveClipsSection)):
+    ExclusiveClips[i] = ExclusiveClipsSection[i].get_attribute()
+    print(ExclusiveClips[i])
 driver.close()
+
