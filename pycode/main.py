e40ae57fd7185a8f1dc9becd27e27c7a152c96bb
Jordan Deuley <jordan.deuley@lyon.edu>
2022-11-09 11:53:04 -0600

    Allowed for section to be scraped. This took me a lot longer than it should of.

--- pycode/main.py
+++ pycode/main.py
@@ -6,22 +6,16 @@ from selenium.webdriver.common.by import By
 
 
 options = Options()
-#options.add_argument("headless") #Comment this out if you want to see it open the browser.
+options.add_argument("headless") #Comment this out if you want to see it open the browser.
 options.add_argument('--disable-gpu')
 driver = webdriver.Chrome(options=options)
 driver.get("https://www.foxnews.com/")
 
-
-
-class article:
-    topic = ""
-    title = ""
-
 ExclusiveClipsSection = driver.find_elements(By.XPATH, "//*[@id='wrapper']/div/div[2]/div[1]/aside[1]/div/div/div[3]/section/div/article")
-ExclusiveClips = ['' for i in range(len(ExclusiveClipsSection))]
+ExclusiveClips = ["" for i in range(len(ExclusiveClipsSection))]
 
-for i in range(0, len(ExclusiveClipsSection)):
-    ExclusiveClips[i] = ExclusiveClipsSection[i].get_attribute()
-    print(ExclusiveClips[i])
+for i in range(1, len(ExclusiveClipsSection)):
+    print(driver.find_element(By.XPATH, "//*[@id='wrapper']/div/div[2]/div[1]/aside[1]/div/div/div[3]/section/div/article[{}]".format(i)).get_attribute("textContent"))
+
 driver.close()
 
