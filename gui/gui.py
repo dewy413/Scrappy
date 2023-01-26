7f4231872160fa8412762aa2251873266bac73aa
Jordan Deuley <114099341+dewy413@users.noreply.github.com>
2023-01-26 11:25:49 -0600

    Implemented search function

--- gui/gui.py
+++ gui/gui.py
@@ -8,22 +8,30 @@ ui = None
 class GUI(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.searchButton = None
         self.ui = ui
         self.loadUI()
-        self.searchButton.clicked.connect(lambda: self.printValue()) #Sets to when the button is press
+        self.searchButton.clicked.connect(lambda: self.searchKeyword()) #Sets to when the button is press
         self.refreshButton.clicked.connect(lambda: self.refresh()) #Sets to when the button is press
-
 
     def loadUI(self):
         self.ui = uic.loadUi("app.ui", self)
         self.show()
 
+    def searchKeyword(self):
+        print("test")
+        listOfArticles = GrabFoxArticles()
+        print("test")
+        desiredArticles = SearchArticles(listOfArticles, str(self.searchEdit.text()))
+        print("test")
+
+        for i in range(len(desiredArticles)):
+            self.searchResults.addItem("\nTitle: " + desiredArticles[i].title + "\nLink: " + desiredArticles[i].link + "\n")
+
+
     def refresh(self):
-        listOfArticle = GrabFoxArticles()
-        self.searchResults.addItems(listOfArticle)
-        for i in range(len(listOfArticle)):
-            self.searchResults.addItem("\nTitle: " + listOfArticle[i].title + "\nLink: " + listOfArticle[i].link + "\n")
+        listOfArticles = GrabFoxArticles()
+        for i in range(len(listOfArticles)):
+            self.searchResults.addItem("\nTitle: " + listOfArticles[i].title + "\nLink: " + listOfArticles[i].link + "\n")
 
 
 def runProgram():
