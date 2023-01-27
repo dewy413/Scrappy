7cee5b326865953da4c9da36fa6f4c7cb1e5da62
Jordan Deuley <114099341+dewy413@users.noreply.github.com>
2023-01-26 20:08:12 -0600

    Slight changes to file.

--- python/scraper.py
+++ python/scraper.py
@@ -2,11 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
-def StartEngine():
-    options = Options()
-    options.add_argument("headless")
-    options.add_argument('--disable-gpu')
-    driver = webdriver.Chrome(options=options)
-    #driver.get("https://www.foxnews.com/")
+class Article:
+    title = ""
+    link = ""
 
-    print("Done")
+
