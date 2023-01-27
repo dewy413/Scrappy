f4ae02a02d45f97e1ca7d9b7de83c7bc50849a25
Jirayu Kaewsing <jirayukaewsing48@gmail.com>
2023-01-27 10:29:17 +0700

    Turn normal Article class into dataclass + optimize GrabFoxArticles

--- python/foxnews.py
+++ python/foxnews.py
@@ -1,29 +1,27 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from dataclasses import dataclass
 
-
+@dataclass
 class Article:
-    title = ""
-    link = ""
+    title: str
+    link: str
 
 
 def GrabFoxArticles():
+    
     options = Options()
     options.add_argument("headless")
     options.add_argument('--disable-gpu')
+    
     driver = webdriver.Chrome(options=options)
     driver.get("https://www.foxnews.com/")
+    
     ListOfArticles = driver.find_elements(By.XPATH, "//article//h2//a")
     del ListOfArticles[0:10:1]
-    FoxNewsArticles = []
-
-    for i in range(len(ListOfArticles)):
-        tempArticle = Article()
-        tempArticle.title = ListOfArticles[i].text
-        tempArticle.link = ListOfArticles[i].get_attribute("href")
-        FoxNewsArticles.append(tempArticle)
-
+    
+    FoxNewsArticles = [Article(article.text, article.getAttribute('href')) for article in ListOfArticles]
     driver.close()
 
     return FoxNewsArticles
