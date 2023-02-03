c2c6854be820e4f182bc9c19266ee263e4042fb6
Jordan Deuley <114099341+dewy413@users.noreply.github.com>
2023-02-03 09:45:33 -0600

    Decided I am going to just focus on perfecting everything with Fox News, then move to other websites.

--- fetcher/cnn.py
+++ fetcher/cnn.py
@@ -4,23 +4,18 @@ from selenium.webdriver.common.by import By
 
 from fetcher.data import Article
 
-
 def GrabCNNArticles() -> list[Article]:
+
     options = Options()
     options.add_argument('headless')
-    # options.add_argument('--disable-gpu')
+    #options.add_argument('--disable-gpu')
 
     driver = webdriver.Chrome(options=options)
     driver.get('https://www.cnn.com/')
-    print("test1")
 
     articles = driver.find_elements(By.CSS_SELECTOR, '.cd__headline')
-    print("test1")
 
-    CNNArticles = [Article(article.find_element(By.TAG_NAME, 'a').text,
-                           article.find_element(By.TAG_NAME, 'a').get_attribute('href')) for article in articles]
-    print("test1")
-
+    CNNArticles = [Article(article.find_element(By.TAG_NAME, 'a').text, article.find_element(By.TAG_NAME, 'a').get_attribute('href')) for article in articles]
     driver.close()
 
     return CNNArticles
