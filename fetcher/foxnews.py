6f1656d6317b5af59ea0a715e26d9c0a1261a342
Jordan Deuley <114099341+dewy413@users.noreply.github.com>
2023-02-20 16:41:31 -0600

    Created duplicate removal. Just need to optimize it a bit.

--- fetcher/foxnews.py
+++ fetcher/foxnews.py
@@ -17,13 +17,22 @@ def GrabFoxArticles() -> list[Article]:
     driver.get("https://www.foxnews.com/")
     
     articles = driver.find_elements(By.XPATH, "//article//h2//a")
-    dupCheck = []
+    removedDupsText = []
+    removedDupsURL = []
+
     del articles[0:10:1]
 
     for i in range(len(articles)):
-        dupCheck.append(i.text)
-        print(i.text)
-    
+        removedDupsText.append(articles[i].text)
+        removedDupsURL.append(articles[i].get_attribute('href'))
+
+    print("List Size Before: ", len(removedDupsText), " ", len(removedDupsURL))
+    removedDupsText = set(removedDupsText)
+    removedDupsURL = set(removedDupsURL)
+    print("List Size Before: ", len(removedDupsText), " ", len(removedDupsURL))
+
+
+
     FoxNewsArticles = [Article(article.text, article.get_attribute('href')) for article in articles]
     driver.close()
 
@@ -35,3 +44,5 @@ def SearchArticles(articles: list[Article], keyword: str) -> list[Article]:
     
     return [article for article in articles if keyword.lower() in article.title.lower()]
 
+
+GrabFoxArticles()
