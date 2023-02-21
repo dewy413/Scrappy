85e57399c892a40388d3977e3d14cd90924672bc
Jordan Deuley <114099341+dewy413@users.noreply.github.com>
2023-02-20 18:18:25 -0600

    Reverted back.

--- fetcher/foxnews.py
+++ fetcher/foxnews.py
@@ -9,29 +9,14 @@ from fetcher.data import Article
 
 def GrabFoxArticles() -> list[Article]:
 
-
     options = Options()
     options.add_argument("headless")
 
     driver = webdriver.Chrome(options=options)
     driver.get("https://www.foxnews.com/")
-    
+
     articles = driver.find_elements(By.XPATH, "//article//h2//a")
-    removedDupsText = []
-    removedDupsURL = []
-
     del articles[0:10:1]
-
-    # for i in range(len(articles)):
-    #     removedDupsText.append(articles[i].text)
-    #     removedDupsURL.append(articles[i].get_attribute('href'))
-    #
-    # print("List Size Before: ", len(removedDupsText), " ", len(removedDupsURL))
-    # removedDupsText = set(removedDupsText)
-    # removedDupsURL = set(removedDupsURL)
-    # print("List Size After: ", len(removedDupsText), " ", len(removedDupsURL))
-    #
-    #
 
     FoxNewsArticles = [Article(article.text, article.get_attribute('href')) for article in articles]
     driver.close()
