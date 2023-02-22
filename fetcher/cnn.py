a09329448cc4f0e2f08f864c62d1a590472eb96f
Jordan Deuley <114099341+dewy413@users.noreply.github.com>
2023-02-22 15:48:24 -0600

    Small adjustments to code.

--- fetcher/cnn.py
+++ fetcher/cnn.py
@@ -8,14 +8,12 @@ def GrabCNNArticles() -> list[Article]:
 
     options = Options()
     options.add_argument('headless')
-    #options.add_argument('--disable-gpu')
+    options.add_argument('--disable-gpu')
 
     driver = webdriver.Chrome(options=options)
     driver.get('https://www.cnn.com/')
 
-    articles = driver.find_elements(By.CSS_SELECTOR, '.cd__headline')
 
-    CNNArticles = [Article(article.find_element(By.TAG_NAME, 'a').text, article.find_element(By.TAG_NAME, 'a').get_attribute('href')) for article in articles]
     driver.close()
 
     return CNNArticles
--- fetcher/foxnews.py
+++ fetcher/foxnews.py
@@ -11,7 +11,7 @@ def GrabFoxArticles() -> list[Article]:
 
     options = Options()
     options.add_argument("headless")
-    #options.add_argument('--disable-gpu')
+    options.add_argument('--disable-gpu')
 
 
     driver = webdriver.Chrome(options=options)
