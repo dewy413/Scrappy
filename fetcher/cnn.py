d963d078c93e48f7584614e6b94c1239f90ae70b
Jordan Deuley <114099341+dewy413@users.noreply.github.com>
2023-03-14 08:41:23 -0500

    Used code that works on WSJ and Fox News. This is good. I just need to make sure it is working perfectly.

--- fetcher/cnn.py
+++ fetcher/cnn.py
@@ -1,13 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 
 from fetcher.data import Article
 
-def GrabCNNArticles() -> list[Article]:
 
+def GrabCNNArticles() -> list[Article]:
     options = Options()
     options.add_argument('headless')
 
@@ -15,17 +13,15 @@ def GrabCNNArticles() -> list[Article]:
     driver.get('https://www.cnn.com/')
     Articles = []
 
-    wait = WebDriverWait(driver, 2)
+    text_grabbing = driver.find_elements(By.XPATH, "//article//h3")
 
-    text_grabbing = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'cd__headline')))
-
-    print(len(text_grabbing))
     for i in range(len(text_grabbing)):
         try:
-            Articles.append(Article(title=text_grabbing[i].text, link=text_grabbing[i].find_element(By.XPATH, 'a').get_attribute('href')))
+            print(text_grabbing[i].text, " ", text_grabbing[i].find_element(By.XPATH, 'a').get_attribute('href'))
+            Articles.append(Article(title=text_grabbing[i].text,
+                                    link=text_grabbing[i].find_element(By.XPATH, 'a').get_attribute('href')))
         except:
             pass
-
 
     driver.close()
 
